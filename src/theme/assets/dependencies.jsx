7f8efbe2987d586f0e98053fbaf3dfab2b6c7055
Glauber Victor Leite Carvalho <glauber.victor@tron.com.br>
2017-12-05 16:39:22 -0200

    Config Antd

--- src/theme/assets/dependencies.jsx
+++ src/theme/assets/dependencies.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 import Messages from '../messages'
 
-//blueprint
-import 'modules/normalize.css/normalize.css'
-import 'modules/@blueprintjs/core/dist/blueprint.css'
-
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 import 'modules/animate.css/animate.min.css'
